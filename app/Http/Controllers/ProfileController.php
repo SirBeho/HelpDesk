82904cde56b78a923448f745e207fda3e41f513c
joregesosa <joregesosa@gmail.com>
2023-11-03 15:57:20 -0400

    fix profile edit bugs

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -34,7 +34,7 @@ class ProfileController extends Controller
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
-
+        unset($request->user()->solicitudes);
         $request->user()->save();
 
         return Redirect::route('profile.edit');
